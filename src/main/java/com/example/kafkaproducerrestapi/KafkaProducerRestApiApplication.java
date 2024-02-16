ede254bc2d110e45b998bc73079af25038e81baf
Alejandro Hierro <alejandrohierro@gmail.com>
2024-02-16 14:28:08 -0300

    initial spring boot project

---
+++ src/main/java/com/example/kafkaproducerrestapi/KafkaProducerRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaproducerrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkaproducerrestapi/KafkaProducerRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaproducerrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
